0d60d09dbb4d0de9b2b8ecdae5f6add3f51889bc
Otavie <otavieokuoyo@gmail.com>
2024-02-15 21:06:52 +0100

    Add code for connecting to database

--- birthday-reminder-backend/public/app.js
+++ birthday-reminder-backend/public/app.js
@@ -5,10 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
 dotenv_1.default.config();
 const PORT = process.env.PORT;
+const DB_URI = process.env.DB_URI;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+mongoose_1.default.Promise = Promise;
+mongoose_1.default.connect(DB_URI);
+mongoose_1.default.connection.on('connected', () => {
+    console.log('Connected to database!');
+});
+mongoose_1.default.connection.on('disconnected', () => {
+    console.log('Disconnected from database!');
+});
+mongoose_1.default.connection.on('error', (error) => {
+    console.log('Error connecting to database:', error);
+});
 app.listen(PORT, () => {
     console.log(`Server is running on PORT http://localhost:${PORT}`);
 });
--- birthday-reminder-backend/src/app.ts
+++ birthday-reminder-backend/src/app.ts
@@ -1,11 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 dotenv.config()
 
 const PORT = process.env.PORT
+const DB_URI = process.env.DB_URI as string
 
 const app = express()
 app.use(express.json())
+
+mongoose.Promise = Promise
+mongoose.connect(DB_URI)
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database!')
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Disconnected from database!')
+})
+
+mongoose.connection.on('error', (error) => {
+    console.log('Error connecting to database:', error)
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT http://localhost:${PORT}`)
