c7157f4c3d37a8d9fa23562123dca1d23f4b8604
Otavie <otavieokuoyo@gmail.com>
2024-02-21 23:50:15 +0100

    add cron code and enabled cors

--- birthday-reminder-backend/public/app.js
+++ birthday-reminder-backend/public/app.js
@@ -1,19 +1,54 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 const db_1 = __importDefault(require("./database/db"));
 const routes_1 = __importDefault(require("./routes/routes"));
+const node_cron_1 = __importDefault(require("node-cron"));
+const celebrants_1 = __importDefault(require("./models/celebrants"));
 dotenv_1.default.config();
 const PORT = process.env.PORT;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+// Enable Cors
+app.use((0, cors_1.default)());
 // Connect to Database
 (0, db_1.default)();
 app.use('/', routes_1.default);
+// cron.schedule('*/5 * * * *', async() => {
+node_cron_1.default.schedule('0 7 * * *', () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const todayDate = new Date();
+        const celebrants = yield celebrants_1.default.find({
+            dateOfBirth: {
+                $gte: new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate()).setHours(0, 0, 0, 0),
+                $lt: new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate() + 1).setHours(0, 0, 0, 0)
+            }
+        });
+        if (celebrants.length > 0) {
+            console.log('Sending birthday email to celebrants:', celebrants);
+        }
+        else {
+            console.log('No birthday today!');
+        }
+    }
+    catch (error) {
+        console.error('Error checking for birthday:', error);
+    }
+}));
 app.listen(PORT, () => {
     console.log(`Server is running on PORT http://localhost:${PORT}`);
 });
--- birthday-reminder-backend/src/app.ts
+++ birthday-reminder-backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import dbConnection from './database/db'
 import routes from './routes/routes'
+import cron from 'node-cron'
+import Celebrants from './models/celebrants'
 dotenv.config()
 
 const PORT = process.env.PORT
@@ -9,10 +12,35 @@ const PORT = process.env.PORT
 const app = express()
 app.use(express.json())
 
+// Enable Cors
+app.use(cors())
+
 // Connect to Database
 dbConnection()
 
 app.use('/', routes)
+
+// cron.schedule('*/5 * * * *', async() => {
+cron.schedule('0 7 * * *', async() => {
+    try {
+        const todayDate = new Date()
+
+        const celebrants = await Celebrants.find({
+            dateOfBirth: {
+                $gte: new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate()).setHours(0, 0, 0, 0),
+                $lt: new Date(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate() + 1).setHours(0, 0, 0, 0)
+            }
+        });
+
+        if (celebrants.length > 0) {
+            console.log('Sending birthday email to celebrants:', celebrants)
+        } else {
+            console.log('No birthday today!')
+        }
+    } catch (error) {
+        console.error('Error checking for birthday:', error)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT http://localhost:${PORT}`)
