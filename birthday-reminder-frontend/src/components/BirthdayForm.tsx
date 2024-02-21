82a316af72feaecb1c6ea3e2d7567295daeb83aa
Otavie <otavieokuoyo@gmail.com>
2024-02-21 23:54:30 +0100

    add date of birth div, submit btn and link backend to frontend

--- birthday-reminder-frontend/src/components/BirthdayForm.tsx
+++ birthday-reminder-frontend/src/components/BirthdayForm.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 const BirthdayForm = () => {
@@ -5,8 +6,28 @@ const BirthdayForm = () => {
   const [email, setEmail] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    try {
+      const response = await axios.post(`http://localhost:5051/birthdays`, {
+        username,
+        email,
+        dateOfBirth,
+      });
+
+      if (response.status === 201) {
+        console.log("celebrants added successfully!");
+        setUsername("");
+        setEmail("");
+        setDateOfBirth("");
+      } else {
+        console.error("Error adding celebrants:", response.data.message);
+      }
+      console.log(response);
+    } catch (error) {
+      console.error("Error adding celebrants:", error);
+    }
 
     setUsername("");
     setEmail("");
@@ -22,6 +43,7 @@ const BirthdayForm = () => {
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            placeholder="enter username"
             required
           />
         </div>
@@ -32,9 +54,24 @@ const BirthdayForm = () => {
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder="enter email"
             required
           />
         </div>
+
+        <div className="label-input-container">
+          <label htmlFor="dateOfBirth">Date of Birth:</label>
+          <input
+            type="text"
+            value={dateOfBirth}
+            onChange={(e) => setDateOfBirth(e.target.value)}
+            placeholder="yyyy-mm-dd"
+          />
+        </div>
+
+        <button className="submit-btn" type="submit" onClick={handleSubmit}>
+          Add Birthday Reminder
+        </button>
       </form>
     </div>
   );
